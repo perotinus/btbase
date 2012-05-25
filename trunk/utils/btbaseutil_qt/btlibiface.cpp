4af9f3c89248a8dc2dc44986b21cea47c635b51c
dvorakviolinist@gmail.com <dvorakviolinist@gmail.com@5160fac3-484b-f1ba-0ab9-892f54608f99>
2012-05-25 21:41:00 +0000

    -Add UI file for BTBaseUtil Qt app -Change BT_QWidget to subclass QMainWindow -Change UI from code based to Qt Designer UI file -Move UI setup/control code to BT_QMainWindow -Add write functionality -Fix wrong mapping between base type writes and written byte -Standardize naming for base types -Fix CMakeLists to account for new UI file and BT_QWidget change

--- trunk/utils/btbaseutil_qt/btlibiface.cpp
+++ trunk/utils/btbaseutil_qt/btlibiface.cpp
@@ -10,8 +10,6 @@ extern "C" {
 
 BTLibIface::BTLibIface() 
 {
-    mm = 0;
-        
     int status = btbaselib_setup();
     
     if (status != BSETUP_OK) {
@@ -23,14 +21,9 @@ BTLibIface::BTLibIface()
 
 }
 
-void 
-BTLibIface::setmm(unsigned char x) 
-{
-        mm = x; 
-}
 
 void 
-BTLibIface::readBase () 
+BTLibIface::readBase() 
 {
     unsigned char btype = btbaselib_readbase();
     
@@ -43,14 +36,14 @@ BTLibIface::readBase ()
 
     QString s = QString("Found base: ");
     switch (btype) {
-        case 0x30: s.append("MED1"); break;
-        case 0x31: s.append("MED2"); break;
-        case 0x40: s.append("AMMO1"); break;
-        case 0x41: s.append("AMMO2"); break;
-        case 0x50: s.append("TBASE1"); break;
-        case 0x51: s.append("TBASE2"); break;
-        case 0x52: s.append("TBASE3"); break;
-        case 0x53: s.append("TBASE4"); break;
+        case 0x30: s.append("MedKit 1"); break;
+        case 0x31: s.append("MedKit 2"); break;
+        case 0x40: s.append("Ammo pack (4 bullets)"); break;
+        case 0x41: s.append("Ammo pack (2 bullets)"); break;
+        case 0x50: s.append("T-Base 1 (red)"); break;
+        case 0x51: s.append("T-Base 2 (blue)"); break;
+        case 0x52: s.append("T-Base 3 (yellow)"); break;
+        case 0x53: s.append("T-Base 4 (green)"); break;
         default: s.append(QString::number(btype, 16)); break;
     }
 
@@ -58,4 +51,42 @@ BTLibIface::readBase ()
     m.setText(s);
     m.exec();
     return;
+}
+
+void 
+BTLibIface::writeBase(unsigned char v) 
+{
+
+    int retval = btbaselib_writebase(v);
+    
+    if (retval == BSETUP_NOBASE) {
+
+        QMessageBox m;
+        m.setText("BattleTag base not found.");
+        m.exec();
+        return;
+
+    } else {
+
+        QMessageBox m;
+        
+        QString s("BattleTag base successfully written: ");
+        switch (v) {
+            case 0x30: s.append("MedKit 1"); break;
+            case 0x31: s.append("MedKit 2"); break;
+            case 0x40: s.append("Ammo pack (4 bullets)"); break;
+            case 0x41: s.append("Ammo pack (2 bullets)"); break;
+            case 0x50: s.append("T-Base 1 (red)"); break;
+            case 0x51: s.append("T-Base 2 (blue)"); break;
+            case 0x52: s.append("T-Base 3 (yellow)"); break;
+            case 0x53: s.append("T-Base 4 (green)"); break;
+            default: s.append(QString::number(btype, 16)); break;
+        }
+
+        m.setText(s);
+        m.exec();
+        return;
+
+    }
+
 }
